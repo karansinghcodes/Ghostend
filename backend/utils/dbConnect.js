24f003417d06b66e45e32479b6a5c4e8f77c21a5
karansinghcodes <karansinghcodes@gmail.com>
2025-07-03 00:49:14 +0530

    added dbConnection check funtion

---
+++ backend/utils/dbConnect.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export async function dbConnection() {
+
+    try {
+        await prisma.$connect();
+        console.info("Databse Connected Successfully");
+
+
+    } catch (error) {
+        console.error("Failed to connect to the database:", error.message);
+        throw new Error(error.message)
+
+    }
+
+}
