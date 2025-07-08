57f2888501107456acc07b4e200bf7d4ba62939e
karansinghcodes <karansinghcodes@gmail.com>
2025-07-08 22:53:31 +0530

    register api with ai

---
+++ backend/controllers/registerApiWithAi.js
@@ -0,0 +1,42 @@
+import { api } from "../router/router.js";
+import { prisma } from "../utils/dbConnect.js";
+import { response } from "../utils/response.js";
+import { generateWithAi } from "../helpers/aiGeneration/openAi.js";
+
+const registerApiWithAI = async (req, res) => {
+  try {
+    const data = req.body;
+
+    const aiResponse = await generateWithAi(data.message);
+
+    if (data) {
+      const date = new Date();
+      const type = aiResponse.type;
+      console.log(type);
+
+      const endPoint = `${date.getTime()}`;
+
+      const newApiData = await prisma.apiData.create({
+        data: {
+          type,
+          endPoint,
+          schema: aiResponse.schema,
+        },
+      });
+
+      if (!newApiData) {
+        console.error("Failed to register new api");
+        return response.error(res, "Failed To Create Api", 400);
+      }
+
+      const apiRoute = `api/${type}/${endPoint}`;
+
+      return response.ok(res, "Created Api Successfully", 201, apiRoute);
+    }
+  } catch (error) {
+    console.error("Api register with ai route Error : ", error.message);
+    return response.error(res, "Internal Server error", 501);
+  }
+};
+
+api.post("/register-api-ai", "noauth", registerApiWithAI);
