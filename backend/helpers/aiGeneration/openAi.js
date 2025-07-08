64be6e769c6d5db0ba875fd20e7fcabe1cf1381f
karansinghcodes <karansinghcodes@gmail.com>
2025-07-08 22:53:00 +0530

    added ai feature to generate data

---
+++ backend/helpers/aiGeneration/openAi.js
@@ -0,0 +1,35 @@
+import { configs } from "../../config/config.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { GoogleGenAI } from "@google/genai";
+import fs from "node:fs";
+import path from "path";
+
+const ai = new GoogleGenAI({ apiKey: configs.geminiApiKey });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.resolve(__dirname, "../../prompt/dataGeneration.txt");
+
+export const generateWithAi = async (userMessage) => {
+  try {
+    const rules = fs.readFileSync(filePath, {
+      encoding: "utf-8",
+    });
+
+    const prompt = rules + userMessage;
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+    });
+
+    const data = JSON.parse(response.text);
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Failed to retrieve data from openAI", error.message);
+  }
+};
